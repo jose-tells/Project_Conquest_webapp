2e74d816fa7a7c16fc76038783e8714c987d2d6b
Jose Marquinez <josemarquinez28@gmail.com>
2022-02-15 15:30:19 -0500

    ⚙️ PROGRESS: Check styles - Component About titles

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -1,7 +1,25 @@
 import React from "react";
 // Components
-import GridAbout from "../components/GridAbout";
+import AboutFormat from "../components/AboutFormat";
+// Media
+import AGUSTIN from "../assets/static/img/Agustin-Blog.jpg";
+import JOSE from "../assets/static/img/mi_cara.jpg";
+import JONNY from "../assets/static/img/Frame-hermanos H.jpg";
+// Styles
+import "../assets/styles/components/GridAbout.styl";
 
-const About = () => <GridAbout />;
+const About = () => (
+  <main className="gridAbout">
+    <AboutFormat media={AGUSTIN} specialty="VFX" position="left" />
+    <AboutFormat
+      media={JOSE}
+      specialty="Filmmaking"
+      specialty2="Photography"
+      position="left"
+    />
+    <AboutFormat media={JONNY} specialty="Illustration" position="right" />
+    <h1 className="gridAbout__title">The Crazy Minds</h1>
+  </main>
+);
 
 export default About;
