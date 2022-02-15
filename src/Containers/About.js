80f338af8bb100551b66647e50c0c4776825cf81
Jose Marquinez <josemarquinez28@gmail.com>
2022-02-15 12:23:54 -0500

    📦 NEW: About Description - New component

--- src/Containers/About.js
+++ src/Containers/About.js
@@ -1,8 +1,6 @@
 import React from "react";
 // Components
 import GridAbout from "../components/GridAbout";
-// Styles
-import "../assets/styles/AboutImports.styl";
 
 const About = () => <GridAbout />;
 
--- src/Containers/Profile.js
+++ src/Containers/Profile.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+// Components
+import ProfileDescription from "../components/ProfileDescription";
 // Redux
 import { getProfile } from "../actions";
+// Media
+import AGUSTIN from "../assets/static/img/Agustin-Blog.jpg";
+// Styles
+import "../assets/styles/Profile.styl";
 
 const Profile = ({ match, getProfile, profile }) => {
   const { name } = match.params;
@@ -13,8 +19,12 @@ const Profile = ({ match, getProfile, profile }) => {
   }, []);
 
   return hasProfile ? (
-    <div>
-      <h1>{profile.description}</h1>
+    <div className="profile__container">
+      <img className="profile__photo" src={AGUSTIN} alt="" />
+      <ProfileDescription
+        name={profile.name}
+        description={profile.description}
+      />
     </div>
   ) : (
     <div>Not found</div>
