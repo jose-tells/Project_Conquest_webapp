9c7f3557bbe81190264c78d765ff1a32a8b9c050
Jose Marquinez <josemarquinez28@gmail.com>
2022-02-15 17:41:00 -0500

    🐛 FIX: Social Media - Social media links styles fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./routes/App";
+import "./assets/styles/Globals.styl";
 // Redux
 import { Provider } from "react-redux";
 import { createStore, compose, applyMiddleware } from "redux";
