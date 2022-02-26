63f9ad22da90c3a5a37eb3fb2cc086058ced237c
Jose Marquinez <josemarquinez28@gmail.com>
2022-02-26 14:15:51 -0500

    🐛 FIX: Toggle Video - Set click event to toggle onPlay event

--- src/components/VideoFormat.js
+++ src/components/VideoFormat.js
@@ -1,42 +1,38 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useInViewEffect } from "react-hook-inview";
 // Styles
 import "@styles/components/VideoFormat.styl";
 
 const VideoFormat = ({ children, source, position }) => {
   const videoRef = React.useRef(null);
 
-  const [isVisible, setVisible] = React.useState(false);
+  const [isPlay, setPlay] = React.useState(false);
 
-  const ref = useInViewEffect(
-    ([entry]) => {
-      setVisible(entry.isIntersecting);
-    },
-    { threshold: 1 }
-  );
-
-  const toggleVideo = (event) => {
-    isVisible && videoRef.current.play();
-    !isVisible && !!event && videoRef.current.pause();
+  const toggleVideo = () => {
+    if (!isPlay) {
+      videoRef.current
+        .play()
+        .then(() => setPlay(true))
+        .catch(console.error);
+    }
+    if (isPlay) {
+      videoRef.current.pause();
+      setPlay(false);
+    }
   };
 
-  React.useEffect(() => {
-    toggleVideo();
-  }, [isVisible]);
-
   return (
-    <div ref={ref} className={`video__container ${position}`}>
+    <div className={`video__container ${position}`}>
       <div>
         <video
-          onPlay={toggleVideo}
+          onClick={toggleVideo}
           className="video__item"
           ref={videoRef}
           muted
-          loop
           src={source}
           type="video/mp4"
           playsInline
+          autoPlay
         />
       </div>
       {children}
